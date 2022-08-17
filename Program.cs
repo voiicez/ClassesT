a0833e81f6f105ea42ba1ff7dbf97cd4656bb501
behlul.sahin <behlul.sahin@ANK5-YZLMORT-02>
2022-08-17 14:15:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ClassesT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ogrenci ogrenci = new Ogrenci(); Not a best way to do it 1
+            Student student = new Student();
+            student.SetId(4);
+            student.SetName("Behlül");
+            student.Surname = "Şahin"; //property kullandık.
+            Console.WriteLine(student.GetId()+" "+student.GetName()+" "+student.Surname);
+            
+            
+        }
+
+    }
+
+    //class Ogrenci Not a best way to do it 1
+    //{
+    //    public int id;
+    //    public string adi;
+    //    public string soyadi; //bunların her biri field, alan.
+    //}
+
+    class Student //Alanları public yapmayalım. "Id" karışmasın diye _id yazalım ilkine.
+    {
+        int _id; //id olduğu durumda this kullanılacak.
+        string _adi;
+        public void SetId(int id) //davranış, behavior.
+        {
+            _id = id; //veya this.id=id; de olurdu.
+        }
+
+        public int GetId() 
+        {
+            return _id;
+        }
+        public void SetName(string adi)
+        {
+            _adi = adi;
+        }
+
+        public string GetName()
+        {
+            return _adi;
+        }
+
+        public string Surname { get; set; } //Otomatik get ve set yapar. özellik,property
+    }
+}
